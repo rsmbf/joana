b0dfb6480156622d9b2f49326257acfecbc362c2
rsmbf <rsmbf@cin.ufpe.br>
2016-01-14 13:30:56 -0300

    Removing inclusion of return type void on signature and including extra  constructor

--- SemanticConflicts/src/main/ModifiedMethod.java
+++ SemanticConflicts/src/main/ModifiedMethod.java
@@ -1,4 +1,5 @@
 package main;
+import java.util.ArrayList;
 import java.util.List;
 
 import edu.kit.joana.ifc.sdg.util.JavaMethodSignature;
@@ -13,13 +14,17 @@ public class ModifiedMethod {
 
 	public ModifiedMethod(String signature, List<String> defaultConstructorArgs, List<Integer> leftContribs, List<Integer> rightContribs, List<String> classImportsList)
 	{
-		this.signature = JavaMethodSignature.fromString("void "+signature);
+		this.signature = JavaMethodSignature.fromString(signature);
 		this.leftContribs = leftContribs;
 		this.rightContribs = rightContribs;
 		this.defaultConstructorArgs = defaultConstructorArgs;
 		this.importsList = classImportsList;
 	}
-
+	
+	public ModifiedMethod(String signature, List<Integer> leftContribs, List<Integer> rightContribs)
+	{
+		this(signature, new ArrayList<String>(), leftContribs, rightContribs, new ArrayList<String>());
+	}
 	public JavaMethodSignature getMethodSignature() {
 		return signature;
 	}
